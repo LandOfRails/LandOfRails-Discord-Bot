bdeb25786cedeea40dd6d9941cc2608bfb9cd741
MarkenJaden <jjsch1410@gmail.com>
2021-01-12 01:17:48 +0100

    Revert "Initial commit"
    This reverts commit eb62c18d

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
-# LandOfSignals Discord Bot
-
-**Simple Discord Bot for the [LandOfSignals Discord Server](https://discord.gg/ykAqHKYjVM)**
-
-Simple, slightly optimized Discord Bot with various features, such as voting, idea management, Fun Command, Roles by
-Reaction, Authentication System, Update commands for
-the [LandOfSignals Launcher](https://github.com/MarkenJaden/LandOfRails-Launcher) and more.
+# LandOfRails Discord Bot
+ **Simple Discord Bot for the [LandOfRails Discord Server](https://discord.gg/hKMrchX)**
+ 
+ Simple, slightly optimized Discord Bot with various features, such as voting, idea management, Fun Command, Roles by Reaction, Authentication System, Update commands for the [LandOfRails Launcher](https://github.com/MarkenJaden/LandOfRails-Launcher) and more.
 
 ## Dependencies
-
 This Discord bot uses the following libraries:
-
 * [JDA (Java Discord API)](https://github.com/DV8FromTheWorld/JDA)
+* [MariaDB Connector/J](https://mariadb.com/kb/en/about-mariadb-connector-j/)
 * [Shadow](https://github.com/johnrengelman/shadow)
+* [Gson](https://github.com/google/gson)
--- src/main/java/commands/handler/CommandList.java
+++ src/main/java/commands/handler/CommandList.java
@@ -1,27 +1,53 @@
 package commands.handler;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import commands.CommandAFK;
+import commands.CommandAbstimmung;
 import commands.CommandFunCommands;
 import commands.CommandHelp;
 import commands.CommandIdea;
 import commands.CommandInfo;
+import commands.CommandRemove;
+import commands.CommandStop;
+import commands.CommandTest;
 import commands.interfaces.Command;
-
-import java.util.ArrayList;
-import java.util.List;
+import commands.launcher.CommandAddModpack;
+import commands.launcher.CommandAddPermission;
+import commands.launcher.CommandRemoveModpack;
+import commands.launcher.CommandRemovePermission;
+import commands.launcher.CommandUpdateImageLink;
+import commands.launcher.CommandUpdateLink;
+import commands.launcher.CommandUpdateVersion;
 
 public class CommandList {
 
-    private List<Command> commands = new ArrayList<>();
+	private List<Command> commands = new ArrayList<>();
 
-    public CommandList() {
-        commands.add(new CommandFunCommands());
-        commands.add(new CommandHelp());
-        commands.add(new CommandIdea());
-        commands.add(new CommandInfo());
-    }
+	public static final CommandAFK AFKCOMMAND = new CommandAFK();
 
-    public List<Command> getCommands() {
-        return commands;
-    }
+	public CommandList() {
+		commands.add(new CommandAbstimmung());
+		commands.add(AFKCOMMAND);
+		commands.add(new CommandFunCommands());
+		commands.add(new CommandHelp());
+		commands.add(new CommandIdea());
+		commands.add(new CommandInfo());
+		commands.add(new CommandRemove());
+		commands.add(new CommandStop());
+		commands.add(new CommandTest());
+		commands.add(new CommandUpdateLink());
+		commands.add(new CommandAddPermission());
+		commands.add(new CommandRemovePermission());
+		commands.add(new CommandUpdateVersion());
+		commands.add(new CommandUpdateImageLink());
+		commands.add(new CommandAddModpack());
+		commands.add(new CommandRemoveModpack());
+	}
+
+	public List<Command> getCommands() {
+		return commands;
+	}
 
 }
